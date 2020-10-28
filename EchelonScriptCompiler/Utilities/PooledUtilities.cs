9218975621141ddbc46ce22ee3ddb96533a8975b
Chronos Ouroboros <doom2.fan@gmail.com>
2020-10-28 17:22:15 -0300

    Changed the license to the MPL 2.0.

--- EchelonScriptCompiler/Utilities/PooledUtilities.cs
+++ EchelonScriptCompiler/Utilities/PooledUtilities.cs
@@ -2,20 +2,10 @@
  * EchelonScript
  * Copyright (C) 2020 Chronos "phantombeta" Ouroboros
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 using System;
 using System.Buffers;
