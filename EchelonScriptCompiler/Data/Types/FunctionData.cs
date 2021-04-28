baab35962f8357755a1966b6f112dc209f1fac4e
Chronos Ouroboros <doom2.fan@gmail.com>
2021-04-28 11:27:15 -0300

    Updated the copyright notice.

--- EchelonScriptCompiler/Data/Types/FunctionData.cs
+++ EchelonScriptCompiler/Data/Types/FunctionData.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * EchelonScript
- * Copyright (C) 2020 Chronos "phantombeta" Ouroboros
+ * Copyright (C) 2020-2021 Chronos "phantombeta" Ouroboros
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
