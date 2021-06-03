9d0c1d0798e090335bc1955d0a15e0e3f03db3a7
Chronos Ouroboros <doom2.fan@gmail.com>
2021-06-03 06:57:19 -0300

    Add a console to the test app.

--- TestSuiteWPF/App.xaml.cs
+++ TestSuiteWPF/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,5 +12,22 @@ namespace TestSuiteWPF {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        [DllImport ("kernel32.dll")]
+        public static extern bool AllocConsole ();
+
+        [DllImport ("kernel32.dll")]
+        public static extern bool FreeConsole ();
+
+        protected override void OnStartup (StartupEventArgs e) {
+            base.OnStartup (e);
+
+            AllocConsole ();
+        }
+
+        protected override void OnExit (ExitEventArgs e) {
+            base.OnExit (e);
+
+            FreeConsole ();
+        }
     }
 }
