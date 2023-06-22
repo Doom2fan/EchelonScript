cadf6604a9fe60458fc48929e712457604154168
Chronos Ouroboros <doom2.fan@gmail.com>
2023-06-22 11:54:44 -0300

    Redo how EchelonScript errors/diagnostics work.

--- EchelonScript.Compiler/Data/Common.cs
+++ EchelonScript.Compiler/Data/Common.cs
@@ -8,10 +8,9 @@
  */
 
 using System;
-using ChronosLib.Pooled;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using EchelonScript.Compiler.CompilerCommon;
-using EchelonScript.Compiler.Frontend.AST;
-using EchelonScript.Compiler.Frontend.Parser.Tokenizer;
 
 namespace EchelonScript.Compiler.Data;
 
@@ -20,63 +19,164 @@ public ref struct SourceData {
     public ReadOnlyMemory<char> FileName;
 }
 
-public struct EchelonScriptErrorMessage {
-    public string? Message { get; }
+public enum ES_DiagnosticSeverity {
+    Info,
+    Warning,
+    Error,
+}
 
-    public int StartPos { get; }
-    public int Length { get; }
+public class ES_DiagnosticDescriptor {
+    #region ================== Instance properties
 
-    public string FileName { get; }
-    public int Line { get; }
-    public int Column { get; }
+    /// <summary>An unique id for the diagnostic.</summary>
+    public string Id { get; private init; }
+    /// <summary>The category of the diagnostic.</summary>
+    public string Category { get; private init; }
 
-    public EchelonScriptErrorMessage (EchelonScriptToken tk, string? message = null) {
-        Message = message;
+    /// <summary>A short message describing the diagnostic.</summary>
+    public string Title { get; private init; }
+    /// <summary>An optional longer message for the diagnostic.</summary>
+    public string? Description { get; private init; }
+    /// <summary>A message string.
+    /// Passed as the first argument to <see cref="string.Format(string, object[])"/> when creating a diagnostic.</summary>
+    public string MessageFormat { get; private init; }
 
-        StartPos = tk.TextStartPos;
-        Length = tk.Text.Length;
+    /// <summary>The diagnostic's severity.</summary>
+    public ES_DiagnosticSeverity DefaultSeverity { get; private init; }
 
-        FileName = tk.FileName.Span.GetPooledString ();
-        Line = tk.TextLine;
-        Column = tk.TextColumn;
+    /// <summary>Whether the user can change the diagnostic's severity or disable it.</summary>
+    public bool IsConfigurable { get; private init; }
+    /// <summary>Whether the diagnostic is enabled by default.</summary>
+    public bool IsEnabledByDefault { get; private init; }
+
+    #endregion
+
+    public ES_DiagnosticDescriptor (
+        string id,
+        string title,
+        string messageFormat,
+        string category,
+        ES_DiagnosticSeverity severity,
+        bool isEnabledByDefault,
+
+        string? description = null,
+        bool isConfigurable = true
+
+    ) {
+        if (!isConfigurable && !isEnabledByDefault)
+            throw new ArgumentException ("Descriptor cannot be both non-configurable and disabled by default.");
+
+        Id = id;
+        Category = category;
+
+        Title = title;
+        Description = description;
+        MessageFormat = messageFormat;
+
+        DefaultSeverity = severity;
+
+        IsConfigurable = isConfigurable;
+        IsEnabledByDefault = isEnabledByDefault;
     }
 
-    public EchelonScriptErrorMessage (ReadOnlySpan<char> srcText, ReadOnlyMemory<char> fileName, ES_AstNodeBounds bounds, string? message = null) {
-        Message = message;
+    #region ================== Static methods
 
-        StartPos = bounds.StartPos;
-        Length = bounds.EndPos - bounds.StartPos;
+    public ES_Diagnostic Create (params object? []? messageArgs) => ES_Diagnostic.Create (this, messageArgs);
 
-        EchelonScriptTokenizer.CalcLine (srcText, StartPos, out var curLine, out var curLineStart);
-        FileName = fileName.Span.GetPooledString ();
-        Line = curLine;
-        Column = EchelonScriptTokenizer.CalcColumn (srcText, curLineStart, StartPos);
+    public ES_Diagnostic Create (SourceLocation? location, params object? []? messageArgs)
+        => ES_Diagnostic.Create (this, location, messageArgs);
+
+    public ES_Diagnostic Create (
+        SourceLocation? location,
+        ImmutableArray<SourceLocation> additionalLocations,
+        params object? []? messageArgs
+    ) => ES_Diagnostic.Create (this, location, additionalLocations, messageArgs);
+
+    #endregion
+}
+
+public struct ES_Diagnostic {
+    #region ================== Instance properties
+
+    private object? [] messageArgs { get; init; }
+
+    /// <summary>The diagnostic's descriptor.</summary>
+    public ES_DiagnosticDescriptor Descriptor;
+
+    /// <summary>The diagnostic's severity.</summary>
+    public ES_DiagnosticSeverity Severity { get; private init; }
+    /// <summary>The diagnostic's default severity.</summary>
+    public readonly ES_DiagnosticSeverity DefaultSeverity => Descriptor.DefaultSeverity;
+
+    /// <summary>The diagnostic's id.</summary>
+    public readonly string Id => Descriptor.Id;
+    /// <summary>The diagnostic's arguments.</summary>
+    public readonly IReadOnlyList<object?> Arguments => messageArgs;
+
+    /// <summary>The diagnostic's location.</summary>
+    public SourceLocation? Location { get; private init; }
+    /// <summary>Any additional locations for the diagnostic.</summary>
+    public ImmutableArray<SourceLocation> AdditionalLocations { get; private init; }
+
+    /// <summary>Whether the diagnostic was suppressed.</summary>
+    public bool IsSuppressed { get; private init; }
+
+    /// <summary>Whether the diagnostic is a warning treated as an error.</summary>
+    public readonly bool IsWarningAsError
+        => DefaultSeverity == ES_DiagnosticSeverity.Warning && Severity == ES_DiagnosticSeverity.Error;
+
+    #endregion
+
+    #region ================== Instance methods
+
+    public ES_Diagnostic WithSeverity (ES_DiagnosticSeverity severity) => this with { Severity = severity, };
+
+    public ES_Diagnostic WithSuppression (bool isSuppressed) => this with { IsSuppressed = isSuppressed, };
+
+    public readonly string GetMessage (IFormatProvider? formatProvider = null) {
+        if (messageArgs.Length < 1)
+            return Descriptor.MessageFormat;
+
+        try {
+            return string.Format (formatProvider, Descriptor.MessageFormat, messageArgs);
+        } catch (Exception) {
+            return Descriptor.MessageFormat;
+        }
     }
 
-    public EchelonScriptErrorMessage (SourceData sourceData, ES_AstNodeBounds bounds, string? message = null)
-        : this (sourceData.Code, sourceData.FileName, bounds, message) { }
+    #endregion
 
-    public EchelonScriptErrorMessage (string message, int startPos, int length, ReadOnlyMemory<char> fileName, int line, int column) {
-        Message = message;
+    #region ================== Static methods
 
-        StartPos = startPos;
-        Length = length;
+    public static ES_Diagnostic Create (
+        ES_DiagnosticDescriptor descriptor,
+        SourceLocation? location,
+        ImmutableArray<SourceLocation> additionalLocations,
+        params object? []? messageArgs
+    ) {
+        return new ES_Diagnostic () with {
+            messageArgs = messageArgs ?? Array.Empty<object?> (),
 
-        FileName = fileName.Span.GetPooledString ();
-        Line = line;
-        Column = column;
+            Descriptor = descriptor,
+            Severity = descriptor.DefaultSeverity,
+
+            Location = location,
+            AdditionalLocations = additionalLocations,
+
+            IsSuppressed = false,
+        };
     }
 
-    public EchelonScriptErrorMessage (string message, ReadOnlySpan<char> fileName) {
-        Message = message;
+    public static ES_Diagnostic Create (
+        ES_DiagnosticDescriptor descriptor,
+        SourceLocation? location,
+        params object? []? messageArgs
+    ) => Create (descriptor, location, ImmutableArray<SourceLocation>.Empty, messageArgs);
 
-        StartPos = -1;
-        Length = -1;
+    public static ES_Diagnostic Create (
+        ES_DiagnosticDescriptor descriptor,
+        params object? []? messageArgs
+    ) => Create (descriptor, null, ImmutableArray<SourceLocation>.Empty, messageArgs);
 
-        FileName = fileName.GetPooledString ();
-        Line = -1;
-        Column = -1;
-    }
-
-    public EchelonScriptErrorMessage (string message, ReadOnlyMemory<char> fileName) : this (message, fileName.Span) { }
+    #endregion
 }
